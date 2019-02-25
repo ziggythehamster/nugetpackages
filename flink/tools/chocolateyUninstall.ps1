641590274ca030aa40f6b9e039b55ee67a6c319b
Keith Gable <ziggy@ignition-project.com>
2019-02-24 18:56:23 -0800

    Uninstall the binfiles I left behind

--- flink/tools/chocolateyUninstall.ps1
+++ flink/tools/chocolateyUninstall.ps1
@@ -1,3 +1,5 @@
+Uninstall-BinFile 'flink'
+Uninstall-BinFile 'pyflink'
 Uninstall-ChocolateyEnvironmentVariable 'FLINK_CONF_DIR'
 Uninstall-ChocolateyEnvironmentVariable 'FLINK_HOME'
 Update-SessionEnvironment
