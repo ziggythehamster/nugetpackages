88b07f29a1b79d453364204f02ad3e9d5936bd58
Keith Gable <ziggy@ignition-project.com>
2019-02-24 19:02:20 -0800

    Update for 10.0.17.

--- db-visualizer/tools/chocolateyInstall.ps1
+++ db-visualizer/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.dbvis.com/product_download/dbvis-10.0.16/media/dbvis_windows_10_0_16.exe'
-$url64      = 'https://www.dbvis.com/product_download/dbvis-10.0.16/media/dbvis_windows-x64_10_0_16.exe'
+$url        = 'https://www.dbvis.com/product_download/dbvis-10.0.17/media/dbvis_windows_10_0_17.exe'
+$url64      = 'https://www.dbvis.com/product_download/dbvis-10.0.17/media/dbvis_windows-x64_10_0_17.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -12,9 +12,9 @@ $packageArgs = @{
 
   softwareName  = 'DbVisualizer*'
 
-  checksum      = '312796B097026D48E27B85ABCBC5C1F3002195086823E7100513084E392F3074'
+  checksum      = 'D213692DA3D7D17C8469A4C7CD0F6B4FA0C5A3AF01081C89D1A115ECAF202B8B'
   checksumType  = 'sha256'
-  checksum64    = 'D1DD43136B11F0D773835D5E68CCF854D1A2EECB5418CA122EE904ADEA1D61EA'
+  checksum64    = 'C2FA038DBD8B376D51565843EFC60BBDCE20378E72AB3E65F20E5A06FA60425C'
   checksumType64= 'sha256'
 
   silentArgs    = "-q"
